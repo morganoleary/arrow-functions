11bf397d946e995c3524fb0dc4728ea8b4e4c6ea
Morgan Oleary <morganleeoleary@gmail.com>
2023-12-31 17:35:42 +0000

    Add code from video tutorial

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,45 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
-// Arrow Function With Parameters
 
-// Single Line Arrow Function With Parameters
+// Arrow Function With Parameters 
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
-// Implicit Returns
+// Single Line Arrow Function With Parameters (simple functions)
+// const addTwoNumbers2 = (a, b) => (a + b); (also valid)
 
-// Returning Multiple Lines
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers(6, 4);
+console.log(sum2);
+
+// Implicit Returns 
+// (1 parameter 'message' doesn't need () )
+
+const saySomething = message => console.log(message);
+saySomething('Hello there!!!');
+
+// (0 parameters, you must add an empty () to tell JS you're declaring a function)
+const sayHello = () => console.log('hello');
+
+// Returning Multiple Lines (empty () required)
+// If you need to return multiple lines of code, you must wrap whatever comes after 
+// the return keyword in ()
+
+const returnMultipleLines = () => {
+    `<p>
+        This is a multiline string!
+    </p>`
+}
+console.log(returnMultipleLines());
